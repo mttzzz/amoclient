e119af8137bff851d6d6324672ac673a86707f50
di <dmitry.vasilevskiy@gmail.com>
2020-09-17 17:28:39 +0300

    add create unsorted form addMetadata

--- src/Entities/Unsorted/Form.php
+++ src/Entities/Unsorted/Form.php
@@ -17,8 +17,21 @@ class Form extends AbstractUnsorted
         $this->entity .= '/forms';
     }
 
-    public function toArray()
+    public function addMetadata($source_uid, $source_name,
+                                $form_id, $form_name, $form_page, $ip, $form_sent_at, $referer,
+                                $pipeline_id = null, $created_at = null)
     {
+        $this->source_uid = $source_uid;
+        $this->source_name = $source_name;
 
+        if ($pipeline_id) {
+            $this->pipeline_id = $pipeline_id;
+        }
+
+        if ($created_at) {
+            $this->created_at = $created_at;
+        }
+        $this->metadata = compact('form_id', 'form_name', 'form_page', 'ip', 'form_sent_at', 'referer');
+        return $this;
     }
 }
