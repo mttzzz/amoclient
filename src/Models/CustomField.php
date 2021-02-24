6ef9a7a4ee930e64ddbdb1e4415514879a7d5e26
di <dmitry.vasilevskiy@gmail.com>
2021-02-24 16:15:44 +0300

    add find to CustomField.php

--- src/Models/CustomField.php
+++ src/Models/CustomField.php
@@ -3,7 +3,9 @@
 
 namespace mttzzz\AmoClient\Models;
 
+use Illuminate\Http\Client\RequestException;
 use mttzzz\AmoClient\Entities;
+use mttzzz\AmoClient\Exceptions\AmoCustomException;
 
 class CustomField extends AbstractModel
 {
@@ -20,5 +22,15 @@ class CustomField extends AbstractModel
     public function entity($id = null)
     {
         return new Entities\CustomField(['id' => $id], $this->http, $this->entity);
+    }
+
+    public function find($id)
+    {
+        try {
+            return $this->http->get($this->entity . '/' . $id)
+                    ->throw()->json() ?? [];
+        } catch (RequestException $e) {
+            throw new AmoCustomException($e);
+        }
     }
 }
