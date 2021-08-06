f71f070c05b4ad6ccfa1288c4586cd1d5a96eb3d
di <dmitry.vasilevskiy@gmail.com>
2021-08-06 17:20:16 +0300

    add with to users

---
+++ src/AmoClientOctane.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace mttzzz\AmoClient;
+
+use Exception;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use mttzzz\AmoClient\Models;
+
+class AmoClientOctane
+{
+    public $leads, $contacts, $companies, $catalogs, $account, $users, $pipelines, $tasks, $events, $ajax,
+        $unsorted, $calls, $webhooks, $shortLinks;
+
+    public function __construct($aId, $clientId = 'a75e6c17-38f5-47f3-a922-ba20e3d22a51') //TODO перевести $clientId (00a140c1-7c52-4563-8b36-03f23754d255) на пушка коннектор
+    {
+        $account = DB::connection(env('DB_CONNECTION_AMO_CLIENT', 'mysql'))->table('accounts')
+            ->select(['accounts.id', 'subdomain', 'account_widget.access_token'])
+            ->join('account_widget', 'accounts.id', '=', 'account_widget.account_id')
+            ->join('widgets', 'widgets.id', '=', 'account_widget.widget_id')
+            ->where('account_widget.active', true)
+            ->where('accounts.id', $aId)
+            ->where('widgets.client_id', $clientId)
+            ->first();
+
+        if (!$account) throw new Exception('Account doesnt active widgets');
+        $account->contact_phone_field_id = null;
+        $account->contact_email_field_id = null;
+
+        $cf = DB::connection(env('DB_CONNECTION_AMO_CLIENT', 'mysql'))
+            ->table('account_custom_fields')
+            ->select('id', 'type')
+            ->where('account_id', $aId)
+            ->get()->pluck('type', 'id')->toArray();
+
+        $http = Http::withToken($account->access_token)
+            ->baseUrl("https://{$account->subdomain}.amocrm.ru/api/v4");
+
+        $this->account = new Models\Account($http);
+        $this->leads = new Models\Lead($http, $cf);
+        $this->contacts = new Models\Contact($http, $account, $cf);
+        $this->companies = new Models\Company($http, $account, $cf);
+        $this->catalogs = new Models\Catalog($http);
+        $this->users = new Models\User($http);
+        $this->pipelines = new Models\Pipeline($http);
+        $this->tasks = new Models\Task($http);
+        $this->events = new Models\Event($http);
+        $this->ajax = new Ajax($account);
+        $this->unsorted = new Models\Unsorted($http);
+        $this->calls = new Models\Call($http);
+        $this->webhooks = new Models\Webhook($http);
+        $this->shortLinks = new Models\ShortLink($http);
+    }
+}
