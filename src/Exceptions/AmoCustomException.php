6f89e9674c85cca5c6871afc9e5b01a5c788f358
di <dmitry.vasilevskiy@gmail.com>
2021-08-09 18:50:31 +0300

    add with to users

--- src/Exceptions/AmoCustomException.php
+++ src/Exceptions/AmoCustomException.php
@@ -9,6 +9,6 @@ class AmoCustomException extends Exception
 {
     public function __construct(RequestException $e)
     {
-        parent::__construct(json_encode(json_decode($e->response->body()), 64 | 128 | 256));
+        parent::__construct(json_encode(json_decode($e->response->body()), 64 | 128 | 256), $e->getCode());
     }
 }
