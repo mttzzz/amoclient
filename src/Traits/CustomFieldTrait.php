037ec9529afe46f3528a2fd553c837f6af8940ad
di <dmitry.vasilevskiy@gmail.com>
2021-09-24 17:14:24 +0300

    fix

--- src/Traits/CustomFieldTrait.php
+++ src/Traits/CustomFieldTrait.php
@@ -55,7 +55,9 @@ trait CustomFieldTrait
                 case 'checkbox':
                     return (bool)$value;
                 case 'birthday':
-                    $value = is_string($value) ? Carbon::parseFromLocale($value, 'ru') : Carbon::createFromTimestamp($value);
+                    $value = is_string($value) ?
+                        Carbon::parseFromLocale($value, preg_match('/[А-Яа-яЁё]/u', $value) ? 'ru' : 'en') :
+                        Carbon::createFromTimestamp($value);
                     return $value->format('Y-m-d\\TH:i:sP');
             }
         }
