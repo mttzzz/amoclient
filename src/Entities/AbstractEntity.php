1730122e066b930fecf918565ff445c37b02047a
di <dmitry.vasilevskiy@gmail.com>
2021-10-22 17:35:08 +0300

    fix

--- src/Entities/AbstractEntity.php
+++ src/Entities/AbstractEntity.php
@@ -21,8 +21,8 @@ abstract class AbstractEntity
     {
         try {
             $intFields = ['id', 'price', 'status_id', 'responsible_user_id', 'duration'];
+            $data['custom_fields_values'] = $data['custom_fields_values'] ?: [];
             foreach ($data as $key => $item) {
-                $item = $key === 'custom_fields_values' ? $item ? : [] : $item;
                 $this->{$key} = (in_array($key, $intFields) && $item) ? (int)$item : $item;
             }
         } catch (Exception $e) {
