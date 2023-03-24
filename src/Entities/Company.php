a757c1e5a993774784483b10727573530642eb29
dionysus <dmitry.vasilevskiy@gmail.com>
2023-03-24 11:21:30 +0300

    Update Company.php

--- src/Entities/Company.php
+++ src/Entities/Company.php
@@ -6,6 +6,7 @@ namespace mttzzz\AmoClient\Entities;
 use Illuminate\Http\Client\PendingRequest;
 use mttzzz\AmoClient\Models;
 use mttzzz\AmoClient\Traits;
+use Illuminate\Support\Arr;
 
 class Company extends AbstractEntity
 {
