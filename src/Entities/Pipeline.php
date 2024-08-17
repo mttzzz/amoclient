1c429c951873cb190cbdfd01663b5de0a5372aa3
Kirill Egorov <kirill_egorov@me.com>
2024-08-17 14:40:46 +0300

    Update Pipeline.php

--- src/Entities/Pipeline.php
+++ src/Entities/Pipeline.php
@@ -9,7 +9,7 @@ class Pipeline extends AbstractEntity
 {
     use Traits\StatusTrait, Traits\CrudEntityTrait;
 
-    protected $entity = 'leads/pipelines', $_embedded = ['statuses' => []];
+    protected $entity = 'leads/pipelines', array $_embedded = ['statuses' => []];
     public $name, $sort = 1, $is_main = false, $is_unsorted_on = true;
 
     public function statuses()
