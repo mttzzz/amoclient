5ec4d989e6e356b8fe2d3a051ac25e0715e5ef3b
Kirill Egorov <kirill_egorov@me.com>
2024-08-17 14:53:56 +0300

    Update Company.php

--- src/Entities/Company.php
+++ src/Entities/Company.php
@@ -15,8 +15,8 @@ class Company extends AbstractEntity
     protected $entity = 'companies';
 
     public $id, $name, $responsible_user_id;
-    public array $custom_fields_values = [];
-    public array $_embedded = [], $notes, $tasks, $links;
+    public $custom_fields_values = [];
+    public $_embedded = [], $notes, $tasks, $links;
 
     public function __construct($data, PendingRequest $http, $cf, $enums)
     {
