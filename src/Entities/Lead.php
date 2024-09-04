1fc641fd0030179ecea667297013cef4c21dbe0b
Kirill Egorov <kirill_egorov@me.com>
2024-09-04 12:45:32 +0300

    update Lead.php

--- src/Entities/Lead.php
+++ src/Entities/Lead.php
@@ -132,7 +132,7 @@ class Lead extends AbstractEntity
         return $companyId ? $this->http->get("companies/$companyId")->json('name') : '';
     }
 
-    public function getPipelineName(): string
+    public function getPipelineName(): ?string
     {
         /** @var OctanePipeline|null $pipeline */
         $pipeline = DB::connection('octane')->table('account_pipelines')
@@ -140,7 +140,7 @@ class Lead extends AbstractEntity
             ->where('id', $this->pipeline_id)
             ->first();
 
-        return $pipeline ? $pipeline->name : '';
+        return $pipeline ? $pipeline->name : null;
     }
 
     /**
